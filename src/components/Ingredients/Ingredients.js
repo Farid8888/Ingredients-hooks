87e317c76156b47fbfcaf7003c191b9029c7bb5d
Farid <faridqasimov8@gmail.com>
2021-08-10 12:13:21 +0400

    use-reducer

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -1,14 +1,51 @@
 import IngredientsInput from "./IngredientsInput";
 import Search from "../Search/Search";
 import IngredientsList from "../Ingredients/IngredientsList";
-import { useState, useEffect,useCallback } from "react";
+import { useState, useEffect, useCallback, useReducer } from "react";
 import React from "react";
 import ErrorModal from "../ui/ErrorModal";
 
+const initialState = {
+  loadingSpinner: false,
+  error: false,
+};
+
+const reducer = (curState, action) => {
+  switch (action.type) {
+    case "SETLOADING":
+      return { ...curState, loadingSpinner: true };
+    case "REMOVELOADING":
+      return { ...curState, loadingSpinner: false };
+    case "SETERROR":
+      return { loadingSpinner: false, error: action.error };
+    case "REMOVEERROR":
+      return { ...curState, error: null };
+    default:
+      throw new Error("shold not get there");
+  }
+};
+
+const ingridientReducer = (prevState, action) => {
+  switch (action.type) {
+    case "SET":
+      return action.ingredients
+    case "ADD":
+      return [...prevState,action.ingredients]
+    case 'FETCH':
+      return action.response  
+    case "DELETE":
+      return prevState.filter(ingredient=>ingredient.id !== action.IdFromFirebase)
+    default:
+      throw new Error("should not get there");
+  }
+};
+
 const Ingredients = () => {
   const [recieveData, setRecieveData] = useState([]);
-  const [loadingSpinner, setLoadingSpinner] = useState(false);
-  const [error,setError] = useState(null)
+  // const [loadingSpinner, setLoadingSpinner] = useState(false);
+  // const [error,setError] = useState(null)
+  const [state, dispatch] = useReducer(reducer, initialState);
+  const [userIngredients, dispatching] = useReducer(ingridientReducer, []);
 
   useEffect(() => {
     fetch("https://auth-with-hooks-default-rtdb.firebaseio.com/form.json")
@@ -22,12 +59,14 @@ const Ingredients = () => {
           };
           responseData.push(newData);
         }
-        setRecieveData(responseData);
+        // setRecieveData(responseData);
+        dispatching({type:'FETCH',response:responseData})
       });
   }, []);
 
   const setData = (sendData) => {
-    setLoadingSpinner(true);
+    // setLoadingSpinner(true);
+    dispatch({ type: "SETLOADING" });
     fetch("https://auth-with-hooks-default-rtdb.firebaseio.com/form.json", {
       method: "POST",
       body: JSON.stringify(sendData),
@@ -37,14 +76,18 @@ const Ingredients = () => {
     })
       .then((response) => response.json())
       .then((data) => {
-        setLoadingSpinner(false);
-        setRecieveData((prevIngredients) => {
-          return [...prevIngredients, { ...sendData, id: data.name }];
-        });
-      }).catch(error=>{
-        setError(error.message)
-        setLoadingSpinner(false)
+        // setLoadingSpinner(false);
+        dispatch({ type: "REMOVELOADING" });
+        dispatching({type:'ADD', ingredients:{...sendData,id:data.name}})
+        // setRecieveData((prevIngredients) => {
+        //   return [...prevIngredients, { ...sendData, id: data.name }];
+        // });
       })
+      .catch((error) => {
+        // setError(error.message)
+        // setLoadingSpinner(false)
+        dispatch({ type: "SETERROR", error: error.message });
+      });
   };
 
   const remove = (IdFromFirebase) => {
@@ -56,30 +99,40 @@ const Ingredients = () => {
     )
       .then((response) => response.json)
       .then((data) => {
-        setRecieveData((prevIngredients) => {
-          return prevIngredients.filter(
-            (ingredient) => ingredient.id !== IdFromFirebase
-          );
-        });
+        // setRecieveData((prevIngredients) => {
+        //   return prevIngredients.filter(
+        //     (ingredient) => ingredient.id !== IdFromFirebase
+        //   );
+        // });
+        dispatching({type:'DELETE',IdFromFirebase:IdFromFirebase})
       });
   };
 
-  const searchHandler = useCallback((fiteredIngredients)=>{
-    setRecieveData(fiteredIngredients)
-  },[])
+  const searchHandler = useCallback((filteredIngredients) => {
+    dispatching({type:'SET',ingredients:filteredIngredients})
+  }, []);
 
-  const closeErrorModal =()=>{
-    setError(null)
-  }
+  const closeErrorModal = () => {
+    // setError(null)
+    dispatch({ type: "REMOVEERROR" });
+  };
 
+  const error = state.error;
+  const loadingSpinner = state.loadingSpinner;
   return (
     <React.Fragment>
-    {error && <ErrorModal error={error} close={closeErrorModal} errorMessage='An Error Occurred!'/>}  
-    <div>
-      <IngredientsInput onSend={setData} loading={loadingSpinner} />
-      <Search search={searchHandler} />
-      <IngredientsList data={recieveData} removeHandler={remove} />
-    </div>
+      {error && (
+        <ErrorModal
+          error={error}
+          close={closeErrorModal}
+          errorMessage="An Error Occurred!"
+        />
+      )}
+      <div>
+        <IngredientsInput onSend={setData} loading={loadingSpinner} />
+        <Search search={searchHandler} />
+        <IngredientsList data={userIngredients} removeHandler={remove} />
+      </div>
     </React.Fragment>
   );
 };
